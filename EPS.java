6daafa222f2f414c260a4135a5841b6f083fd3d2
David Vanegas <david-vanegas@javeriana.edu.co>
2020-05-17 13:18:20 -0500

    Nuevo

--- EPS.java
+++ EPS.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -7,9 +10,12 @@ public class EPS implements iEPS {
     String nombre;
     HashMap<String, Usuario> usuarios = new HashMap<String, Usuario>();
 
-    public EPS(String nombre){
-        this.nombre = nombre;
+    public EPS(String[] args) throws RemoteException, NotBoundException {
+        this.nombre = args[0];
         listaEPS();
+        Registry registry = LocateRegistry.getRegistry(args[1], 49154);
+        iIPS epsNueva = (iIPS) registry.lookup("ips");
+        epsNueva.nuevaEPS(args[2], this.nombre);
     }
 
     public void listaEPS() {
@@ -32,5 +38,5 @@ public class EPS implements iEPS {
     @Override
     public boolean verificarExistencia(String documento) throws RemoteException {
         return this.usuarios.containsKey(documento);
-    }    
+    }
 }
